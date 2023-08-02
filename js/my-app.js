2285c937f29d4691de2d133e56acbc8d6dab6ba9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-08-02 10:45:05 +0700

    komponen

--- js/my-app.js
+++ js/my-app.js
@@ -21,6 +21,23 @@ var app = new Framework7({
 
 var mainView = app.views.create(".view-main");
 
+var $$ = Dom7;
+
+// Create full-layout notification
+var notificationFull = app.notification.create({
+  icon: '<i class="icon demo-icon">7</i>',
+  title: "Notification",
+  titleRightText: "now",
+  subtitle: "This is a message",
+  text: "Welcome!",
+  closeTimeout: 3000,
+});
+
+// Open Notifications
+$$(".open-full").on("click", function () {
+  notificationFull.open();
+});
+
 var calendarDefault = app.calendar.create({
   inputEl: "#demo-calendar-default",
 });
